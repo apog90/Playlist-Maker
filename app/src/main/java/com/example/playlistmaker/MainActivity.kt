6e68635702dbb92462477fceef7b3c99a1f8cd68
Anastasia Pogorelova <a.pogorelova.fw@gmail.com>
2026-05-03 02:52:26 +0300

    Спринт 8: добавлена навигация на главном экране и частично в настройках

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,15 +14,22 @@ class MainActivity : AppCompatActivity() {
         val searchButton = findViewById<Button>(R.id.search_button)
         val searchClickListener: View.OnClickListener = object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                Toast.makeText(this@MainActivity, "Заглушка клика на Поиск", Toast.LENGTH_SHORT).show()
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchIntent)
             }
         }
         searchButton.setOnClickListener(searchClickListener)
 
         val mediaButton = findViewById<Button>(R.id.media_button)
-        mediaButton.setOnClickListener { Toast.makeText(this@MainActivity, "Заглушка клика на Медиатеку", Toast.LENGTH_SHORT).show() }
+        mediaButton.setOnClickListener {
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
 
         val settingsButton = findViewById<Button>(R.id.settings_button)
-        settingsButton.setOnClickListener { Toast.makeText(this@MainActivity, "Заглушка клика на Настройки", Toast.LENGTH_SHORT).show() }
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }

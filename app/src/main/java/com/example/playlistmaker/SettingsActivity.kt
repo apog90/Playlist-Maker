7060d338ba0b6912ab6fd652d7d50a97e8672b52
Anastasia Pogorelova <a.pogorelova.fw@gmail.com>
2026-05-07 08:49:30 +0300

    Спринт 9: добавлены intent шэринга, пользовательского соглащения и поддержки

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,12 +1,18 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
 
 class SettingsActivity : AppCompatActivity() {
+    val supportEmail = "apog2im@gmail.com"
+    val userAgreementUrl = "https://yandex.ru/legal/practicum_offer/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,6 +26,31 @@ class SettingsActivity : AppCompatActivity() {
         val toolbar = findViewById<com.google.android.material.appbar.MaterialToolbar>(R.id.toolbar)
         toolbar.setNavigationOnClickListener {
             finish()
+        }
+
+        val shareItem = findViewById<LinearLayout>(R.id.shareItem)
+        shareItem.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text))
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share)))
+        }
+
+        val supportItem = findViewById<LinearLayout>(R.id.supportItem)
+        supportItem.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = "mailto:".toUri()
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(supportEmail))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_subject))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.support_text))
+            startActivity(supportIntent)
+        }
+
+        val userAgreementItem = findViewById<LinearLayout>(R.id.userAgreementItem)
+        userAgreementItem.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW,
+                userAgreementUrl.toUri())
+            startActivity(agreementIntent)
         }
     }
 }
